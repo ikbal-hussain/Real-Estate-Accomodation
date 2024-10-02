86d00b3cc2858c7e017002cdba5cad773c98bd1f
PradyumnaJadhav11 <pradyu.jadhav@gmail.com>
2024-10-02 20:53:44 +0530

    welcome_section

--- real_estate_app/src/pages/Home.jsx
+++ real_estate_app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header';
 
 import Featured from '../components/Featured'
+import Welcome from '../components/Welcome';
 // import Amenities from '../components/Amenities'
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
   return (
     <div>
         <Header/>
+        <Welcome/>
       <Featured />
       {/* <Amenities /> */}
     </div>
