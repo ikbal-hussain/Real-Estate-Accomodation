c4218859f4c58a60ff3939fb465f19c62904a992
Farhan Ali Ansari <alifarhan095@gmail.com>
2024-10-02 18:19:19 +0530

    Signup and Login Page

--- real_estate_app/src/App.jsx
+++ real_estate_app/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from './pages/Home'
 
-function App() {
- 
-
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
