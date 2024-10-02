d1774b301e4f53e2750df5459822664158b90e29
Ikbal Hussain <ikbalhussain3674@gmail.com>
2024-10-02 19:41:17 +0530

    issues fixed

--- real_estate_app/src/App.jsx
+++ real_estate_app/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
