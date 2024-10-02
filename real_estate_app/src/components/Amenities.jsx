7b3591e1c0ab2a070d56a88f1c2f849f7ec364d6
Ikbal Hussain <ikbalhussain3674@gmail.com>
2024-10-02 20:15:26 +0530

    issues fixed

--- real_estate_app/src/components/Amenities.jsx
+++ real_estate_app/src/components/Amenities.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+
+
+
+
+
+
 const Amenities = ({ imgPath, amenityName }) => {
+
+
+ 
+
+
+
   return (
+    <>
     <div className='amenityCard'>
       <div className='imgContainer'>
         <img src={imgPath} alt={amenityName} />
@@ -8,7 +21,28 @@ const Amenities = ({ imgPath, amenityName }) => {
       <div className='textContainer'>
         <h3>{amenityName}</h3>
       </div>
+
+
+
     </div>
+  <section className='amenitiesSection'>
+  <div className='amenitiesTextContainer'>
+    <h3>Our Amenities</h3>
+    <p>Building Amenities</p>
+  </div>
+
+  <div className='amenitiesCardContainer'>
+    {amenitiesData.map((amenity, index) => (
+      <Amenities
+        key={index}
+        imgPath={amenity.imgPath}
+        amenityName={amenity.amenityName}
+      />
+    ))}
+  </div>
+</section>
+    </>
+    
   );
 };
 
--- real_estate_app/src/pages/Home.jsx
+++ real_estate_app/src/pages/Home.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from '../components/Header';
 
 import Featured from '../components/Featured'
-import Amenities from '../components/Amenities'
+// import Amenities from '../components/Amenities'
 
 function Home() {
+
+
   return (
     <div>
         <Header/>
       <Featured />
-      <Amenities />
+      {/* <Amenities /> */}
     </div>
   )
 }
