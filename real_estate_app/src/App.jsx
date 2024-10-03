60fe4d7bbce11bd1be9fbc68417b883b253c3f64
Farhan Ali Ansari <alifarhan095@gmail.com>
2024-10-03 19:09:19 +0530

    about section

--- real_estate_app/src/App.jsx
+++ real_estate_app/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from './pages/Home'
-
 import './App.css'
+import AboutUs from "./pages/AboutUs";
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<AboutUs />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
